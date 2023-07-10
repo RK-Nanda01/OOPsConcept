31f1fa28c3e80a8d15c7dd2363230c28387ca097
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-10 16:53:09 +0530

    [RK Nanda] Adding Employee Class

---
+++ OOPsConcept/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace OOPsConcept
+{
+	public class Employee
+	{
+		private
+			string name;
+			int age;
+
+		public Employee(string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		public void GetDeatils()
+		{
+			Console.WriteLine($"The name of the employee is {this.name} and age is {this.age}");
+
+		}
+	}
+}
+
---
+++ OOPsConcept/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPsConcept;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("OOPS Concept");
+        Employee e1 = new Employee("RAMAN" , 32);        
+        e1.GetDeatils();
+    }
+}
+
